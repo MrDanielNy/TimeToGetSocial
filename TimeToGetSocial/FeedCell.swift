e1de49ed62c387d358f9cd47f39a834ac57a1753
MrDanielNy <tjanster1@danielny.se>
2017-07-03 17:03:13 +0200

    UI Part 2, the Feed Cell

---
+++ TimeToGetSocial/FeedCell.swift
@@ -0,0 +1,27 @@
+//
+//  FeedCell.swift
+//  TimeToGetSocial
+//
+//  Created by Daniel Ny on 2017-07-03.
+//  Copyright © 2017 Daniel Ny. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: CircleView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+
+}
--- TimeToGetSocial/FeedVC.swift
+++ TimeToGetSocial/FeedVC.swift
@@ -10,12 +10,18 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
 
@@ -24,5 +30,22 @@ class FeedVC: UIViewController {
         try! Auth.auth().signOut()
         performSegue(withIdentifier: "goToSignin", sender: nil)
     }
+    
+    //It´s a must for tableView!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! FeedCell
+        
+    }
+    
 
 }
