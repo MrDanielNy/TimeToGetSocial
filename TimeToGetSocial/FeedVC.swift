8be770adfbf7c1bed41d6b5483d79a4fca9e525d
MrDanielNy <tjanster1@danielny.se>
2017-07-04 12:27:52 +0200

    Implemented ImagePicker

--- TimeToGetSocial/FeedVC.swift
+++ TimeToGetSocial/FeedVC.swift
@@ -10,26 +10,60 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAdd: CircleView!
+    
+    var imagePicker: UIImagePickerController!
 
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
         
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
         DataSerivce.ds.REF_POSTS.observe(.value , with: { (snapshot) in
-            print(snapshot.value!)
+            if let postsFromDB = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in postsFromDB {
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
+        
     }
 
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageAdd.image = image
+        } else {
+            print("ADGJMP: Did not select a valid image")
+        }
+        dismiss(animated: true, completion: nil)
+
+    }
     
     @IBAction func signoutBtnTapped(_ sender: Any) {
         KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         try! Auth.auth().signOut()
         performSegue(withIdentifier: "goToSignin", sender: nil)
+    }
+    
+    
+    @IBAction func addImageTapped(_ sender: Any) {
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     //It´s a must for tableView!
@@ -39,12 +73,21 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! FeedCell
+        
+        let post = posts[indexPath.row]
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? FeedCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return FeedCell()
+        }
+        
+     //   return cell
         
     }
     
